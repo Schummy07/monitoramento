017f640b5757b301bc623f2269489fe892d280da
Felipe Ferri <felipe.ferri07@gmail.com>
2026-05-27 19:42:08 -0300

    primeiro deploy

---
+++ app.py
@@ -0,0 +1,63 @@
+import funcs as fc
+import dash
+from dash import html
+from dash import dcc
+from dash import Input
+from dash import Output
+
+app = dash.Dash(__name__)
+
+
+app.layout = html.Div([
+    
+    html.Div([html.H1("Mapa Analítico"),
+    dcc.Dropdown(id = "Parametro_Operacional", 
+                  options = [{"label": "Tonelagem", "value": "producao"},
+                             {"label": "Tempo_Coleta", "value": "horas_coleta"},
+                             {"label": "Tamanho: km percorrido em coleta", "value": "km_coleta"}, 
+                             {"label": "Produtividade: ton/h", "value": "ton/h"},
+                             {"label": "Densidade: ton/km", "value": "ton/km"}],
+                  value = "producao",
+                  multi = False),
+    dcc.DatePickerRange(id = "filtro_data",
+                        start_date = "2025-05-20",
+                        end_date = "2025-06/20"),
+    dcc.Dropdown(id = "Turno",
+                 options = [{"label": "Noturno", "value": "NOTURNO"},
+                            {"label": "Diurno", "value": "DIURNO"}],
+                 value = ["DIURNO"],
+                 multi = True), 
+    dcc.Dropdown(id = "Frequencia",
+                 options = [{"label":"Segunda", "value": "seg"},
+                            {"label":"Terca", "value": "ter"},
+                            {"label":"Quarta", "value": "qua"},
+                            {"label":"Quinta", "value": "qui"},
+                            {"label":"Sexta", "value": "sex"},
+                            {"label":"Sabado", "value": "sáb"}],
+                 value = ["seg","ter","qua","qui","sex","sáb","dom"],
+                 multi = True)], 
+             style = {"justifyContent": "center", "width": "60%" }),
+    
+    html.Div([dcc.Graph(id = "grafico")], 
+             style = {"display": "flex", "justifyContent": "center", "marginTop": "30px", "marginBottom": "50px"})
+], 
+                      style={
+        "display": "flex",
+        "flexDirection": "column",   
+        "alignItems": "center",      
+        "mainHeight": "100vh", 
+        "paddingBottom": "50px"
+    })
+
+def atualizar(variavel, data_ini, data_fin, turno, dia_semana):
+    return fc.mapa_analitico(variavel, data_ini, data_fin, turno, dia_semana)
+
+app.callback(Output("grafico", "figure"),
+             Input("Parametro_Operacional", "value"),
+             Input("filtro_data", "start_date"),
+             Input("filtro_data", "end_date"),
+             Input("Turno", "value"),
+             Input("Frequencia", "value"))(atualizar)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ funcs.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import plotly.express as px 
+import geopandas as gps
+
+def mapa_analitico(variavel, data_ini, data_fin, turno, dia_semana):
+    # coreção da variável para se ajustar ao projeto 
+    #variavel = variavel[0]
+    
+    #importação dos dados
+    dados = pd.read_excel("dados.xlsx", sheet_name= "Dados")
+    dados["data"] = pd.to_datetime(dados["data"], dayfirst= True)
+    mapa = gps.read_file("mapa_atual.geojson")
+    
+    data_ini = pd.to_datetime(data_ini)
+    data_fin = pd.to_datetime(data_fin)
+    
+    #aplicação dos filtros 
+    dados_analise = dados[(dados["data"]>= data_ini) &
+                          (dados["data"]<= data_fin) &
+                          (dados["turno"].isin(turno)) &
+                          (dados["dia_semana"].isin(dia_semana))]
+    
+    #calculo das médias da variável selecionada para cada setor 
+    medias = dados_analise.groupby("setor", as_index = False)[variavel].mean()
+    medias["setor"] = [f"0{i}" if i <10 else f"{i}" for i in medias["setor"]]
+    
+    #aplicação do filtro no arquivo shp 
+    mapa_filtro = mapa[mapa["SETOR"].isin(medias["setor"])]
+    mapa_plot = mapa_filtro.__geo_interface__
+    
+    # return mapa_filtro, medias - linha de teste 
+    # plot do gráfico georeferenciado 
+    fig = px.choropleth_map(
+        medias,
+        geojson=mapa_plot,
+        locations="setor",
+        featureidkey="properties.SETOR",
+        color= variavel,
+        color_continuous_scale="Oranges",
+        center = {"lat": -22.39, "lon": -47.577}, 
+        zoom = 11, 
+        opacity = 0.5)
+    
+    fig.update_layout(
+        margin=dict(l=8, r=0, t=0, b=0), 
+        width = 900, 
+        height = 900)
+    
+    return fig
+    
