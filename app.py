5011da8d01ba4db043d5633c298c3c0d1ae08a0b
Felipe Ferri <felipe.ferri07@gmail.com>
2026-06-04 19:33:31 -0300

    adição de gráficos no dashboard

--- app.py
+++ app.py
@@ -7,10 +7,20 @@ from dash import Output
 
 app = dash.Dash(__name__)
 
-
 app.layout = html.Div([
     
-    html.Div([html.H1("Mapa Analítico"),
+    html.Div([html.H1("Painel Analítico: Rio Claro - SP")],
+             style = {"position": "fixed",
+                      "display": "flex",
+                      "zIndex": "9999",
+                      "backgroundColor": "#27672F",
+                      "top": "0",
+                      "width": "100%",
+                      "justifyContent": "center",
+                      "marginLeft": "13px",
+                      "marginRight": "13px"}),
+    
+    html.Div([html.H1("Filtros"),
     dcc.Dropdown(id = "Parametro_Operacional", 
                   options = [{"label": "Tonelagem", "value": "producao"},
                              {"label": "Tempo_Coleta", "value": "horas_coleta"},
@@ -21,11 +31,11 @@ app.layout = html.Div([
                   multi = False),
     dcc.DatePickerRange(id = "filtro_data",
                         start_date = "2025-05-20",
-                        end_date = "2025-06/20"),
+                        end_date = "2025-06-20"),
     dcc.Dropdown(id = "Turno",
                  options = [{"label": "Noturno", "value": "NOTURNO"},
                             {"label": "Diurno", "value": "DIURNO"}],
-                 value = ["DIURNO"],
+                 value = ["DIURNO", "NOTURNO"],
                  multi = True), 
     dcc.Dropdown(id = "Frequencia",
                  options = [{"label":"Segunda", "value": "seg"},
@@ -36,30 +46,157 @@ app.layout = html.Div([
                             {"label":"Sabado", "value": "sáb"}],
                  value = ["seg","ter","qua","qui","sex","sáb","dom"],
                  multi = True)], 
-             style = {"justifyContent": "center", "width": "60%" }),
+             style = {"display":"flex",
+                      "justifyContent": "center",
+                      "flexDirection": "column",
+                      "width": "60%", 
+                      "marginTop": "70px"}),
     
-    html.Div([dcc.Graph(id = "grafico")], 
-             style = {"display": "flex", "justifyContent": "center", "marginTop": "30px", "marginBottom": "50px"})
+    html.Div([html.H2("Mapa de Calor", style = {"marginLeft": "20px"}),
+        html.Div([dcc.Graph(id = "mapa")], 
+             style = {"display": "flex",
+                      "justifyContent": "center",
+                      "marginTop": "30px",
+                      "marginBottom": "50px",
+                      "marginLeft": "20px",
+                      "marginRight": "20px"})],
+             style = {"backgroundColor": "#9feaa8",
+                      "marginTop": "30px",
+                      "marginBottom":"30px",
+                      "border":"5px solid black"}),
+    
+    html.Div([html.H1("Série Histórica", style = {"marginLeft": "20px"}),
+        html.Div([dcc.Graph(id = "grafico1", style = {"width": "48%"}), dcc.Graph(id = "grafico2", style = {"width": "48%"})], 
+             style = {"display":"flex",
+                      "justifyContent": "center",
+                      "flexDirection": "row", 
+                      "gap": "20px",
+                      "width": "100%", 
+                      "marginBottom": "30px"})],
+             style = {"border":"5px solid black",
+                      "backgroundColor": "#9feaa8",
+                      "width": "90%",
+                      "marginBottom": "50px",
+                      "justifyContent": "center"}),
+    
+    html.Div([html.H1("Correlação", style = {"marginLeft": "20px"}),
+        html.Div([dcc.Dropdown(id = "variavel 1",
+                               options = [{"label":"Tamanho_setor", "value": "km_coleta"},
+                                           {"label": "Deslocamento", "value": "km_trajeto"},
+                                           {"label": "Tonelagem", "value": "producao_ton"},
+                                           {"label": "Produtividade: ton/h", "value": "ton/h"},
+                                           {"label": "Densidade: ton/km", "value": "ton/km"}],
+                               value = "producao_ton"),
+                  dcc.Dropdown(id = "variavel 2",
+                               options = [{"label":"Tamanho_setor", "value": "km_coleta"},
+                                           {"label": "Deslocamento", "value": "km_trajeto"},
+                                           {"label": "Tonelagem", "value": "producao_ton"},
+                                           {"label": "Produtividade: ton/h", "value": "ton/h"},
+                                           {"label": "Densidade: ton/km", "value": "ton/km"}],
+                               value = "ton/h"),
+                  dcc.DatePickerRange(id = "filtro_data_1",
+                                      start_date = "2025-05-20",
+                                      end_date = "2025-06-20"),
+                  dcc.Dropdown(id = "Frequencia_1", 
+                               options = [{"label":"Segunda", "value": "seg"},
+                                          {"label":"Terca", "value": "ter"},
+                                          {"label":"Quarta", "value": "qua"},
+                                          {"label":"Quinta", "value": "qui"},
+                                          {"label":"Sexta", "value": "sex"},
+                                          {"label":"Sabado", "value": "sáb"}],
+                               value = ["seg"],
+                               multi = True),
+                  dcc.Dropdown(id = "Turno_1", 
+                               options = [{"label":"Noturno", "value":"NOTURNO"},
+                                           {"label": "Diurno", "value": "DIURNO"}],
+                               value = ["DIURNO","NOTURNO"], 
+                               multi = True),
+                  dcc.Dropdown(id = "Setor",
+                               options = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21],
+                               value = [9,18,16,14,13,20],
+                               multi = True)], 
+                 style = {"display": "flex", 
+                          "flexDirection": "column", 
+                          "justifyContent": "center", 
+                          "width": "60%"}),
+        html.Div([dcc.Graph(id = "correlacao", style = {"width": "65%"}), dcc.Graph(id = "histograma", style = {"width": "30%"})],
+                 style = {"display": "flex",
+                          "flexDirection": "row",
+                          "justifyContent": "center",
+                          "gap" : "20px" ,
+                          "width": "100%",
+                          "marginBottom": "30px",
+                          "marginTop": "15px"})],
+             style = {"display": "flex", 
+                      "flexDirection": "column", 
+                    # "justifyContent": "center",
+                      "backgroundColor": "#9feaa8",
+                      "width": "90%", 
+                      "alignItems": "center",
+                      "border":"5px solid black"})
 ], 
-                      style={
-        "display": "flex",
-        "flexDirection": "column",   
-        "alignItems": "center",      
-        "mainHeight": "100vh", 
-        "paddingBottom": "50px"
-    })
+                      style={"display": "flex",
+                             "flexDirection": "column",
+                             "alignItems": "center",
+                             "margin": "0",
+                             "padding": "0",
+                             "backgroundColor": "#26ce34"})
 
-def atualizar(variavel, data_ini, data_fin, turno, dia_semana):
+def gerar_mapa(variavel, data_ini, data_fin, turno, dia_semana):
     return fc.mapa_analitico(variavel, data_ini, data_fin, turno, dia_semana)
 
-app.callback(Output("grafico", "figure"),
+def gerar_grafico1(variavel, data_ini, data_fin, turno, dia_semana):
+    return fc.serie_historica(variavel, data_ini, data_fin, turno, dia_semana)
+
+def gerar_grafico2(variavel, data_ini, data_fin, turno, dia_semana):
+    return fc.medias(variavel, data_ini, data_fin, turno, dia_semana)
+
+def gerar_correlacao(variavel1, variavel2, data_ini, data_fin, dia_semana, turno, setor):
+    return fc.correl(variavel1, variavel2, data_ini, data_fin, dia_semana, turno, setor)
+
+def gerar_histograma(variavel1, variavel2, data_ini, data_fin, dia_semana, turno, setor):
+    return fc.histograma(variavel1, variavel2, data_ini, data_fin, dia_semana, turno, setor)
+
+app.callback(Output("mapa", "figure"),
              Input("Parametro_Operacional", "value"),
              Input("filtro_data", "start_date"),
              Input("filtro_data", "end_date"),
              Input("Turno", "value"),
-             Input("Frequencia", "value"))(atualizar)
+             Input("Frequencia", "value"))(gerar_mapa)
+
+app.callback(Output("grafico1", "figure"),
+             Input("Parametro_Operacional", "value"),
+             Input("filtro_data", "start_date"),
+             Input("filtro_data", "end_date"),
+             Input("Turno", "value"),
+             Input("Frequencia", "value"))(gerar_grafico1)
+
+app.callback(Output("grafico2", "figure"),
+             Input("Parametro_Operacional", "value"),
+             Input("filtro_data", "start_date"),
+             Input("filtro_data", "end_date"),
+             Input("Turno", "value"),
+             Input("Frequencia", "value"))(gerar_grafico2)
+
+app.callback(Output("correlacao", "figure"),
+             Input("variavel 1", "value"),
+             Input("variavel 2", "value"),
+             Input("filtro_data_1", "start_date"),
+             Input("filtro_data_1", "end_date"),
+             Input("Frequencia_1", "value"),
+             Input("Turno_1", "value"),
+             Input("Setor", "value"),)(gerar_correlacao)
+
+app.callback(Output("histograma", "figure"),
+             Input("variavel 1", "value"),
+             Input("variavel 2", "value"),
+             Input("filtro_data_1", "start_date"),
+             Input("filtro_data_1", "end_date"),
+             Input("Frequencia_1", "value"),
+             Input("Turno_1", "value"),
+             Input("Setor", "value"),)(gerar_histograma)
 
 server = app.server 
 
 if __name__ == "__main__":
-    app.run_server(host = "0.0.0.0", port=8050)
+    app.run(host = "0.0.0.0", port=8050)
--- funcs.py
+++ funcs.py
@@ -1,6 +1,8 @@
 import pandas as pd
-import plotly.express as px 
+import plotly.express as px
+import plotly.graph_objects as go 
 import geopandas as gps
+import numpy as np 
 
 def mapa_analitico(variavel, data_ini, data_fin, turno, dia_semana):
     # coreção da variável para se ajustar ao projeto 
@@ -23,7 +25,8 @@ def mapa_analitico(variavel, data_ini, data_fin, turno, dia_semana):
                           (dados["dia_semana"].isin(dia_semana))]
     
     #calculo das médias da variável selecionada para cada setor 
-    medias = dados_analise.groupby("setor", as_index = False)[variavel].mean()
+    medias = dados_analise.groupby(by = ["data" ,"setor"], as_index = False)[variavel].sum()
+    medias = medias.groupby("setor", as_index = False)[variavel].mean()
     medias["setor"] = [f"0{i}" if i <10 else f"{i}" for i in medias["setor"]]
     
     #aplicação do filtro no arquivo shp 
@@ -50,3 +53,101 @@ def mapa_analitico(variavel, data_ini, data_fin, turno, dia_semana):
     
     return fig
     
+    
+def serie_historica(variavel, data_ini, data_fin, turno, dia_semana):
+    data = pd.read_csv("https://raw.githubusercontent.com/Schummy07/monitoramento/main/dados.csv")
+    data["data"] = pd.to_datetime(data["data"])
+    
+    data_ini = pd.to_datetime(data_ini)
+    data_fin = pd.to_datetime(data_fin)
+    
+    data_filtro = data[(data["data"] >= data_ini) & (data["data"]<= data_fin)]
+    data_grafico = data_filtro[(data_filtro["turno"].isin(turno)) & (data_filtro["dia_semana"].isin(dia_semana))]
+    data_grafico = data_grafico.groupby(by = ["data", "setor"], as_index = False)[variavel].sum()
+    
+    figura = px.line(data_grafico, x = "data", y = variavel, color = "setor")
+    
+    return figura
+
+
+def medias(variavel, data_ini, data_fin, turno, dia_semana):
+    data = pd.read_csv("https://raw.githubusercontent.com/Schummy07/monitoramento/main/dados.csv")
+    data["data"] = pd.to_datetime(data["data"])
+    
+    data_ini = pd.to_datetime(data_ini)
+    data_fin = pd.to_datetime(data_fin)
+    
+    data_filtro = data[(data["data"] >= data_ini) & (data["data"]<= data_fin)]
+    data_grafico = data_filtro[(data_filtro["turno"].isin(turno)) & (data_filtro["dia_semana"].isin(dia_semana))]
+    
+    data_grafico = data_grafico.groupby(by = ["data", "setor", "dia_semana"], as_index = False)[variavel].sum()
+    data_grafico = data_grafico.groupby( by = ["setor", "dia_semana"], as_index = False)[variavel].mean()
+    
+    figura = px.bar(data_grafico, x = variavel, y = "setor", color = "dia_semana", orientation = "h")
+    figura.update_layout(barmode = "group")
+    
+    return figura
+
+
+def correl(variavel1, variavel2, data_ini, data_fin, dia_semana, turno, setor):
+    dados = pd.read_csv("https://raw.githubusercontent.com/Schummy07/monitoramento/main/dados.csv")
+    dados["data"] = pd.to_datetime(dados["data"])
+    
+    data_ini = pd.to_datetime(data_ini)
+    data_fin = pd.to_datetime(data_fin)
+    
+    dados_filtro = dados[(dados["data"] >= data_ini) 
+                   & (dados["data"]<= data_fin) 
+                   & (dados["dia_semana"].isin(dia_semana))
+                   & (dados["turno"].isin(turno))
+                   & (dados["setor"].isin(setor))]
+    
+    dados_grafico = dados_filtro.groupby(by = ["data", "setor"], as_index = False).aggregate(x = (variavel1,"sum"),
+                                                                                             y = (variavel2, "sum"))
+    
+    x = dados_grafico["x"]
+    y = dados_grafico["y"]
+    coefs = np.polyfit(x, y, 1)
+    pred = coefs[0]*x + coefs[1]
+    
+    figura = go.Figure()
+    figura.add_trace(go.Scatter(x = x, y = y, mode = "markers", name = "Observações"))
+    figura.add_trace(go.Scatter(x = x, y = pred, mode = "lines", name = "Regressão"))
+    
+    R = round(np.corrcoef(x, y)[0][1],2)
+    figura.add_annotation(xref = "paper",
+                          yref = "paper",
+                          x = 0.9,
+                          y = 0.9,
+                          text = f" y = {round(coefs[0],2)}x + {round(coefs[1],2)}<br> R = {R}")
+    
+    return figura
+    
+    
+def histograma(variavel1, variavel2, data_ini, data_fin, dia_semana, turno, setor):
+    dados = pd.read_csv("https://raw.githubusercontent.com/Schummy07/monitoramento/main/dados.csv")
+    dados["data"] = pd.to_datetime(dados["data"])
+    
+    data_ini = pd.to_datetime(data_ini)
+    data_fin = pd.to_datetime(data_fin)
+    
+    dados_filtro = dados[(dados["data"] >= data_ini) 
+                   & (dados["data"]<= data_fin) 
+                   & (dados["dia_semana"].isin(dia_semana))
+                   & (dados["turno"].isin(turno))
+                   & (dados["setor"].isin(setor))]
+    
+    dados_grafico = dados_filtro.groupby(by = ["data", "setor"], as_index = False).aggregate(x = (variavel1,"sum"),
+                                                                                             y = (variavel2, "sum"))
+    
+    x = dados_grafico["x"]
+    y = dados_grafico["y"]
+    coefs = np.polyfit(x, y, 1)
+    pred = coefs[0]*x + coefs[1]
+    residuo = np.abs(dados_grafico["y"] - pred)
+    
+    figura = go.Figure()
+    figura.add_trace(go.Histogram(x = residuo, name = "Residuo"))
+    
+    
+    return figura
